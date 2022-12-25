4c0d66fef39f129d3087c5efefbb8c7ac849b775
Maxim Chernyshov <dsgnrvd@gmail.com>
2022-12-25 22:02:04 +0400

    add preloaded font component

---
+++ components/PreloadedFont.tsx
@@ -0,0 +1,16 @@
+import localFont from "@next/font/local";
+import { cloneElement, createElement, ReactElement, ReactHTML } from "react";
+
+const mulishFont = localFont({ src: '../public/fonts/Mulish-Regular.ttf' });
+
+type IPreloadedFont = {
+    children: ReactElement | string;
+    variant: keyof ReactHTML;
+    className: string;
+};
+
+export const PreloadedFont = ({ children, variant, className }: IPreloadedFont) => {
+    return createElement(variant, {
+        className: `${mulishFont.className} ${className}`,
+    }, children);
+}
