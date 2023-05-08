78c407efbdcb30e44e1785eb9bf301fed73bc3a7
Maxim Chernyshov <dsgnrvd@gmail.com>
2023-05-08 17:02:03 +0400

    Revert "split with ~"
    This reverts commit c2c2d4e98fd57f623d8fae089e577816b03a06aa.

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,8 +29,7 @@ export default function Home() {
   const perhapsConnectTwitter = useCallback(async () => {
     if (typeof perhapsTwitterState === 'string') {
       try {
-        const [domain, tgData] = perhapsTwitterState.split('~')
-        console.log(domain)
+        const [domain, tgData] = perhapsTwitterState.split('+')
 
         await ky.post(`${domain}/validate`, {
           json: {
